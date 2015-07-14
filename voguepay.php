2bed2bfbda9f7d727701cf42799a954dc86eb067
Joseph Dahryl Cobhams <jcobhams@vyrenmedia.com>
2015-07-14 15:15:38 +0200

    First Upload

---
+++ voguepay.php
@@ -0,0 +1,12 @@
+<?php
+/**********************************************
+ * AN UNOFFICIAL VOGUEPAY API WRAPPER WRITTEN IN PHP 5 OOP STYLE
+ *
+ * Name:        VoguePay API Wrapper
+ * Version:     1.0
+ * Author:      Cobhams, Joseph
+ * URL:         http://www.vyrenmedia.com (Vyren Media)
+ * License:
+ *
+ */
+?>
